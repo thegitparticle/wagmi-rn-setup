f60b25553faa952be9a9ba47436e3121e2e8dafd
thegitparticle <maddamsettysanjit@gmail.com>
2022-09-17 22:45:00 +0530

    add: expo-crypto-polygfills, walletconnect provider to entire App

--- App.js
+++ App.js
@@ -2,16 +2,25 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import WalletComponent from "./src/components/WalletComponent";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-// import WalletConnectProvider from "@walletconnect/react-native-dapp";
+import WalletConnectProvider from "@walletconnect/react-native-dapp";
 
 export default function App() {
 	return (
-		// <WalletConnectProvider>
-		<View style={styles.container}>
-			<WalletComponent />
-			<StatusBar style="auto" />
-		</View>
-		// </WalletConnectProvider>
+		<WalletConnectProvider
+			redirectUrl={
+				Platform.OS === "web"
+					? window.location.origin
+					: "wagmi-rn-setup://"
+			}
+			storageOptions={{
+				asyncStorage: AsyncStorage,
+			}}
+		>
+			<View style={styles.container}>
+				<WalletComponent />
+				<StatusBar style="auto" />
+			</View>
+		</WalletConnectProvider>
 	);
 }
 
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Note: It is being built from a Dapp point of view, not wallet.
 
 ##### Dos, Don'ts and more
+
+1. WalletConnect will need js crypto module which is not supported by RN by default. So, do `expo install expo-crypto-polyfills` and change the `metro.config.js` as done in this repo.
