1cdd31fa42d1c02a1e0cfb818ea139688bf156dc
thegitparticle <maddamsettysanjit@gmail.com>
2022-09-18 23:00:00 +0530

    add: connect wagmi logic - getProvider() issue

--- src/components/WalletComponent.js
+++ src/components/WalletComponent.js
@@ -1,9 +1,30 @@
 import { useWalletConnect } from "@walletconnect/react-native-dapp";
-import React from "react";
+import React, { useEffect } from "react";
 import { Pressable, Text, View, StyleSheet } from "react-native";
+import { useAccount } from "wagmi";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 
 function WalletComponent() {
 	const connector = useWalletConnect();
+
+	const { connect } = useConnect({
+		connector: new WalletConnectConnector({
+			options: {
+				qrcode: false,
+				connector,
+			},
+		}),
+	});
+
+	useEffect(() => {
+		if (connector?.accounts?.length && !account) {
+			connect();
+		} else {
+			disconnect();
+		}
+	}, [connector]);
+
+	const { data: account } = useAccount();
 
 	function WalletButton() {
 		if (connector.connected) {
@@ -21,10 +42,29 @@ function WalletComponent() {
 		}
 	}
 
+	function AccountDetails() {
+		if (connector.connected && account) {
+			return (
+				<View>
+					<Text style={styles.account}>{account.address}</Text>
+				</View>
+			);
+		} else {
+			return (
+				<View>
+					<Text style={styles.account}>
+						wagmi not showing account
+					</Text>
+				</View>
+			);
+		}
+	}
+
 	return (
 		<View>
 			<Text>Wallet</Text>
 			<WalletButton />
+			<AccountDetails />
 		</View>
 	);
 }
