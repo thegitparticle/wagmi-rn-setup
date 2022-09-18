bd484afe014933c229118b07acaf7edc8b6ff927
thegitparticle <maddamsettysanjit@gmail.com>
2022-09-18 22:11:10 +0530

    add: wagmi, ethers and wagmi config wrap around App

--- App.js
+++ App.js
@@ -5,24 +5,40 @@ import { StyleSheet, Text, View } from "react-native";
 import WalletComponent from "./src/components/WalletComponent";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import WalletConnectProvider from "@walletconnect/react-native-dapp";
+import { WagmiConfig, createClient, createStorage } from "wagmi";
+import { noopStorage } from "@wagmi/core";
+import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
+
+const asyncStoragePersister = createAsyncStoragePersister({
+	storage: AsyncStorage,
+});
+
+const client = createClient({
+	persister: asyncStoragePersister,
+	storage: createStorage({
+		storage: noopStorage,
+	}),
+});
 
 export default function App() {
 	return (
-		<WalletConnectProvider
-			redirectUrl={
-				Platform.OS === "web"
-					? window.location.origin
-					: "wagmi-rn-setup://"
-			}
-			storageOptions={{
-				asyncStorage: AsyncStorage,
-			}}
-		>
-			<View style={styles.container}>
-				<WalletComponent />
-				<StatusBar style="auto" />
-			</View>
-		</WalletConnectProvider>
+		<WagmiConfig client={client}>
+			<WalletConnectProvider
+				redirectUrl={
+					Platform.OS === "web"
+						? window.location.origin
+						: "wagmi-rn-setup://"
+				}
+				storageOptions={{
+					asyncStorage: AsyncStorage,
+				}}
+			>
+				<View style={styles.container}>
+					<WalletComponent />
+					<StatusBar style="auto" />
+				</View>
+			</WalletConnectProvider>
+		</WagmiConfig>
 	);
 }
 
